3bf29cc0f35fc1d4b9fc2f0ea7376c2648e9848b
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-20 12:16:48 +0000

    move dependency injection into a separate file

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 var crypto = require('crypto');
 
-exports.__browserify = function (crypto, exports) {
-  exports = exports || {};
-  var ciphers = require('./encrypter')(crypto);
-  exports.createCipher = ciphers.createCipher;
-  exports.createCipheriv = ciphers.createCipheriv;
-  var deciphers = require('./decrypter')(crypto);
-  exports.createDecipher = deciphers.createDecipher;
-  exports.createDecipheriv = deciphers.createDecipheriv;
-  var modes = require('./modes');
-  function listCiphers () {
-    return Object.keys(modes);
-  }
-  exports.listCiphers = listCiphers;
-};
+exports.__browserify = require('./inject')
 exports.__browserify(crypto, module.exports);
