688c706b98d4c643f5f2142022f9aa6fa121b489
Calvin Metcalf <cmetcalf@appgeo.com>
2015-01-05 11:07:47 -0500

    add aliases

--- inject.js
+++ inject.js
@@ -1,11 +1,11 @@
 module.exports = function (crypto, exports) {
   exports = exports || {};
   var ciphers = require('./encrypter')(crypto);
-  exports.createCipher = ciphers.createCipher;
-  exports.createCipheriv = ciphers.createCipheriv;
+  exports.createCipher = exports.Cipher = ciphers.createCipher;
+  exports.createCipheriv = exports.Cipheriv = ciphers.createCipheriv;
   var deciphers = require('./decrypter')(crypto);
-  exports.createDecipher = deciphers.createDecipher;
-  exports.createDecipheriv = deciphers.createDecipheriv;
+  exports.createDecipher = exports.Decipher = deciphers.createDecipher;
+  exports.createDecipheriv = exports.Decipheriv = deciphers.createDecipheriv;
   var modes = require('./modes');
   function getCiphers () {
     return Object.keys(modes);
