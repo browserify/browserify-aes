383983431c09a70324a693fc9e516f8ebb4ea5cb
Daniel Cousens <github@dcousens.com>
2017-05-19 13:37:38 +1000

    add benchsuite

---
+++ bench/index.js
@@ -0,0 +1,32 @@
+let Benchmark = require('benchmark')
+let _local = require('../browser')
+let _npm = require('browserify-aes/browser')
+let key = Buffer.alloc(16, 0xff)
+let iv = Buffer.alloc(16, 0x01)
+
+function test (mod, message) {
+  let cipher = mod.createCipheriv('aes-128-cbc', key, iv)
+  let b = cipher.update(message)
+  return Buffer.concat([b, cipher.final()])
+}
+
+let local = (m) => test(_local, m)
+let npm = (m) => test(_npm, m)
+
+function run (message) {
+  if (
+    local(message).toString('hex') !==
+    npm(message).toString('hex')
+  ) throw new Error('not equal')
+
+  new Benchmark.Suite()
+  .add('local', () => local(message))
+  .add('npm', () => npm(message))
+  .on('cycle', (e) => console.log(String(e.target)))
+  .run()
+}
+
+let lorem = Buffer.allocUnsafe(800)
+run(lorem.slice(0, 20), key)
+run(lorem.slice(0, 80), key)
+run(lorem, key)
